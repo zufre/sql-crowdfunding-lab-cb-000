ae6316a1126997ea1e0938834b17dc3ed7a9438a
Cedric David <cedric.david@btopenworld.com>
2018-09-10 05:20:39 +0000

    Done.

--- lib/create.sql
+++ lib/create.sql
@@ -1,19 +1,20 @@
 CREATE TABLE projects (
-  id INTEGER PRIMARY KEY,
-  title TEXT,
-  category TEXT,
-  funding_goal INTEGER,
-  start_date TEXT,
-  end_date TEXT
-);
+                        id INTEGER PRIMARY KEY,
+                        title TEXT,
+                        category TEXT,
+                        funding_goal DECIMAL,
+                        start_date TEXT,
+                        end_date TEXT
+                      );
+
 CREATE TABLE users (
-  id INTEGER PRIMARY KEY,
-  name TEXT,
-  age INTEGER
-);
+                    id INTEGER PRIMARY KEY,
+                    name TEXT,
+                    age INTEGER
+                  );
 CREATE TABLE pledges (
-  id INTEGER PRIMARY KEY,
-  amount INTEGER,
-  user_id INTEGER,
-  project_id INTEGER
-);
+                    id INTEGER PRIMARY KEY,
+                    amount DECIMAL,
+                    user_ID INTEGER,
+                    project_ID Integer
+                  );
