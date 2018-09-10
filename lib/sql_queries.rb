3392031414901d70f97261e665eb5baf143f87c0
Cedric David <cedric.david@btopenworld.com>
2018-09-10 05:35:45 +0000

    Done.

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -7,7 +7,7 @@
 # Make sure each ruby method returns a string containing a valid SQL statement.
 
 def selects_the_titles_of_all_projects_and_their_pledge_amounts_alphabetized_by_name
-"Write your SQL query Here"
+"SELECT projects.title, pledges.amount FROM projects INNER JOIN pledges ON projects.id = pledges.project_ID ORDER BY projects.title;"
 end
 
 def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_name
