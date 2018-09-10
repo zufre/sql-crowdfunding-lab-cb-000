c4dba5518f3b735b8b6c8e11dc938138e8d91d09
Cedric David <cedric.david@btopenworld.com>
2018-09-10 11:27:37 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -44,6 +44,6 @@ def selects_the_category_name_and_the_sum_total_of_the_all_its_pledges_for_the_b
 FROM projects
 JOIN pledges
 ON projects.id = pledges.project_ID
-WHERE project.category IS 'books';
+WHERE projects.category IS 'books';
 "
 end
